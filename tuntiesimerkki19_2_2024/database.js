3ebcd1fab728b0dadd0848fcd356cc96dc639178
Reetamari Heikkinen <reetamariheikkinen@gmail.com>
2024-02-27 20:50:41 +0200

    Poistettu tiedosto

--- tuntiesimerkki19_2_2024/database.js
+++
@@ -1,8 +0,0 @@
-const mysql = require('mysql2');
-const connection = mysql.createPool({
-  host: 'localhost',
-  user: 'netuser',
-  password: 'netpass',
-  database: 'netdb'
-});
-module.exports = connection;
